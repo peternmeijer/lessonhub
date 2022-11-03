c24e8a1acad90e5f2fbf08cd96e273e493d23fb3
Nathaniel Pogue <npogue@uwo.ca>
2022-11-03 16:16:48 -0400

    adding pages

--- front-end/lessonhub/src/App.js
+++ front-end/lessonhub/src/App.js
@@ -1,13 +1,27 @@
+import React from "react";
 import Navigation from "./components/Navigation/Navigation";
-import Welcome from "./components/Welcome/Welcome"
+import Home from "./components/Home/Home";
+import Calendar from "./components/Calendar/Calendar";
+import Lessons from "./components/Lessons/Lessons";
+import About from "./components/About/About";
+
 import ParticlesBg from "particles-bg";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <ParticlesBg type = "cobweb" num = {200} color="#FFFFFF" bg={true} />
-      <Navigation />
-      <Welcome />
+      <Router>
+        <ParticlesBg type="cobweb" num={200} color="#FFFFFF" bg={true} />
+        <Navigation />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/calendar' element={<Calendar/>} />
+          <Route path='/lessons' element={<Lessons/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
