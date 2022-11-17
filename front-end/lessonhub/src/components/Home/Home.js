d6822bb87730136a98eaddb7c868d73b86e7942d
Nathaniel Pogue <npogue@uwo.ca>
2022-11-17 15:44:44 -0500

    added lesson and activity page

--- front-end/lessonhub/src/components/Home/Home.js
+++ front-end/lessonhub/src/components/Home/Home.js
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div style={{ flexDirection: "column", display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh' }}>
-            <div>
-                <h1>Welcome to Lesson Hub</h1>
+        <>
+            <div style={{ flexDirection: "column", display: 'flex', alignItems: 'center', justifyContent: 'center', height: '90vh' }}>
+                <div>
+                    <h1>Welcome to Lesson Hub</h1>
+                </div>
+                <div>
+                    <h5>Build and share lesson plans.</h5>
+                    <h5>From anywhere. For anyone.</h5>
+                </div>
+                <div>
+                    <Link to="/lessons">
+                        <button>Create Lesson</button>
+                    </Link>
+                </div>
             </div>
-            <div>
-                <p>Blah Blah Blah subheading here</p>
-            </div>
-            <div>
-                <Link to="/lessons">
-                    <button>Create Lesson</button>
-                </Link>
-            </div>
-        </div>
+        </>
     );
 }
 
