0e9f1bc122122ebacebe2699bb25b814a322d6fc
Nathaniel Pogue <npogue@uwo.ca>
2022-11-23 23:52:59 -0500

    adding search functionality

--- front-end/lessonhub/src/components/Lessons/Lessons.js
+++ front-end/lessonhub/src/components/Lessons/Lessons.js
@@ -1,28 +1,38 @@
 import React from "react";
 import LessonCardList from "../LessonCardList/LessonCardList";
+import SearchBox from "../SearchBox/SearchBox";
 
 class Lessons extends React.Component {
 
-    constructor(){
-		super();
-		this.state = {
-			lessonData: [],
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            lessonData: [],
+            searchfield: ''
+        }
+    }
 
-    componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({lessonData: users}));
-	}
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ lessonData: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
 
     render() {
 
-        const { lessonData } = this.state;
+        const { lessonData, searchfield } = this.state;
+		const filteredLessons = lessonData.filter(lessonData =>{
+			return lessonData.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
 
         return (
             <div className='tc'>
-                <LessonCardList lessonData={lessonData} />
+                <SearchBox searchChange={this.onSearchChange} />
+                <LessonCardList lessonData={filteredLessons} />
             </div>
 
         );
