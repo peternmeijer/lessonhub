1e99d8c8ebe595df8b594406fdcf625fa465952f
Peter Meijer <pmeijer@gefconsulting.com>
2022-11-23 23:45:38 -0500

    cookies

--- back-end/routes/lesson.js
+++ back-end/routes/lesson.js
@@ -7,7 +7,7 @@ const {getLesson, createLesson, updateLesson} = require('../controllers/lesson')
 //middleware
 const {checkAccess, accountTypeGuard} = require("../middleware/accessVerification");
 
-//routes
+//lesson sub-routes
 router.route("/:id").get(checkAccess, accountTypeGuard("Instructor"), getLesson);
 router.route("/").post(checkAccess, accountTypeGuard("Instructor"), createLesson);
 router.route("/:id").patch(checkAccess, accountTypeGuard("Instructor"), updateLesson);
