d04c36a10a4d9cd60f6607122c081b9946dca698
Nathaniel Pogue <npogue@uwo.ca>
2022-11-27 23:49:24 -0500

    adding about page

--- front-end/lessonhub/src/components/Navigation/Navigation.js
+++ front-end/lessonhub/src/components/Navigation/Navigation.js
@@ -5,32 +5,33 @@ import { React, useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../contexts/user.context";
 import { Button } from '@mui/material';
+import logo from '../../assets/logo-color.png'
 
 const Navigation = () => {
   const { logOutUser } = useContext(UserContext);
 
 
-    const logOut = async () => {
-      try {
-        // Calling the logOutUser function from the user context.
-        const response = await logOutUser();
-        window.location.reload(true);
-        localStorage.removeItem("user")
-        // Now we will refresh the page, and the user will be logged out and
-        // redirected to the login page because of the <PrivateRoute /> component.
-        
-      } catch (error) {
-        alert(error)
-      }
+  const logOut = async () => {
+    try {
+      // Calling the logOutUser function from the user context.
+      const response = await logOutUser();
+      window.location.reload(true);
+      localStorage.removeItem("user")
+      // Now we will refresh the page, and the user will be logged out and
+      // redirected to the login page because of the <PrivateRoute /> component.
 
+    } catch (error) {
+      alert(error)
     }
-  
+
+  }
+
 
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
         <Container>
-          <Navbar.Brand as={Link} to="/">Lesson Hub</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/"><img src={logo} style={{width: '250px'}}></img></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
